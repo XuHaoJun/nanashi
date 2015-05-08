3f11bd0ddc99fe83f51a60399ced409d4f41d78c
XuHaoJun <XuHaoJunTW@gmail.com>
2015-05-09 00:53:00 +0800

    Fix typo.

---
+++ config/db.js
@@ -0,0 +1,34 @@
+var dbConfig = {
+  develop: {
+    client: 'pg',
+    mysql: {
+      connection: {
+        host     : 'localhost',
+        user     : 'root',
+        password : 'yourPassword',
+        database : 'nanashi'
+      }
+    },
+    pg: {
+      connection: 'postgres://postgres:postgres@localhost/nanashi'
+    }
+  },
+  production: {
+    client: 'pg',
+    pg: {
+      connection: process.env.HEROKU_POSTGRESQL_TEAL_URL,
+      pool: {
+        min: 0,
+        max: 5
+      }
+    }
+  }
+};
+
+var _config = dbConfig.develop;
+if (dbConfig.production !== undefined && process.env.NODE_ENV == 'production') {
+  _config = dbConfig.production;
+}
+_config = {client: _config.client, connection: _config[_config.client].connection};
+
+module.exports = _config;
